5e8ba8c795eb10e7bb6886accb7cbef5110235f3
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2017-05-24 12:22:55 +0200

    Adding script for upgrading project to newest boilerplate tag.

--- boilerplate/scripts/upgrade.js
+++ boilerplate/scripts/upgrade.js
@@ -1,19 +1,40 @@
 const simpleGit = require('simple-git')();
-const argv = require('yargs').argv;
 
 const logger = require('../logger');
-const project = require('../project');
-const packageInfo = require(project.getPath('package.json'));
 
 logger.info('Upgrading boilerplate...');
 
-console.log(argv);
 simpleGit.status((_, status) => {
-  // if (status.files.length) {
-  //   logger.warn(
-  //     'There are uncommited files in your repository!\r\nCommit your work before upgrading.'
-  //   );
-  // } else {
-  simpleGit.listRemote(['--tags', 'origin'], console.log);
-  // }
+  if (status.files.length) {
+    logger.warn(
+      'There are uncommited files in your repository!\r\nCommit your work before upgrading.'
+    );
+  } else {
+    simpleGit.fetch(['--tags', 'upgrade'], error => {
+      if (error) {
+        logger.error(error);
+      }
+      logger.info('Fetching tags from "upgrade" remote.');
+
+      simpleGit.listRemote(['--tags', 'upgrade'], (error, rawTags) => {
+        if (error) {
+          logger.error(error);
+        }
+        const tags = rawTags
+          .split('\n')
+          .filter(Boolean)
+          .map(ref => ref.match(/tags\/([^\\]+)/)[1])
+          .reverse();
+        logger.debug('Found following tags:', tags);
+        const newestTag = tags[0];
+        logger.info(`Merging newest tag "${newestTag}"...`);
+        simpleGit.raw(['merge', newestTag], (error, info) => {
+          if (error) {
+            logger.error(error);
+          }
+          logger.info(info);
+        });
+      });
+    });
+  }
 });
